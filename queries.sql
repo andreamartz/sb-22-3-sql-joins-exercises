9fadf488cc25313b94c88ac3d0fabc59c4d1304c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-17 19:49:48 -0500

    Add query #1

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
 -- write your queries here
 -- Join the two tables so that every column and record appears, regardless of if there is not an owner_id
-
+SELECT * FROM owners 
+  FULL JOIN vehicles 
+    ON owners.id = vehicles.owner_id;
 
 -- Count the number of cars for each owner. Display the owners first_name, last_name and count of vehicles. The first_name should be ordered in ascending order. 
 
